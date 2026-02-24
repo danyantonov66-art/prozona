03eacaf89b6f02b022dc786d8d20dc2f5552edf5
Yordan Antonov <danyantonov66@gmail.com>
2026-02-24 15:27:23 +0200

    Add gallery page content

---
+++ app/[locale]/specialist/gallery/page.tsx
@@ -0,0 +1,8 @@
+export default function GalleryPage() {
+  return (
+    <div style={{ padding: '2rem', color: 'white', background: '#0D0D1A', minHeight: '100vh' }}>
+      <h1>Галерия</h1>
+      <p>Добре дошли в галерията на специалистите!</p>
+    </div>
+  )
+}
--- locales/[locale]/specialist/gallery/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function GalleryPage() {
-  return (
-    <div>
-      <h1>Галерия</h1>
-    </div>
-  )
-}
