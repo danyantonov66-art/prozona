7adaa0d19e4e567deae762aaa8ffdd32cfa7d81f
Yordan Antonov <danyantonov66@gmail.com>
2026-02-24 13:36:52 +0200

    Rename middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -19,7 +19,7 @@ function getLocale(request: NextRequest): string {
   return match(languages, locales, defaultLocale)
 }
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
 
   // Провери дали пътят вече съдържа език
