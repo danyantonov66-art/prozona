eb7ccdd3d08a5eb6ca3203c14403c83b3844470b
Yordan Antonov <danyantonov66@gmail.com>
2026-02-27 07:21:27 +0200

    Fix prisma schema - full models

---
+++ check-tables.js
@@ -0,0 +1,7 @@
+﻿const { PrismaClient } = require('./node_modules/@prisma/client');
+const prisma = new PrismaClient();
+async function main() {
+  const count = await prisma.categorySuggestion.count();
+  console.log('CategorySuggestion count:', count);
+}
+main().catch(console.error).finally(() => process.exit());
