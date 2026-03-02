e1957461bd9b0b86a9b0724ad0f9e059af4037a5
danyantonov66 <danyantonov66@gmail.com>
2026-03-02 08:39:38 +0200

    fix: optimize build for vercel internal error

---
+++ next.config.js
@@ -0,0 +1,15 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+    ],
+  },
+  // Ако имаш специфични настройки за i18n, добави ги тук
+};
+
+module.exports = nextConfig;
