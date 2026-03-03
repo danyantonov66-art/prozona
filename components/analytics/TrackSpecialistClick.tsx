134e2e4eb54f59d9eee3cdb2e722de93b3f55d32
danyantonov66 <danyantonov66@gmail.com>
2026-03-03 16:14:55 +0200

    feat: add specialist click tracking

---
+++ components/analytics/TrackSpecialistClick.tsx
@@ -0,0 +1,37 @@
+﻿"use client";
+
+import { useCallback } from "react";
+
+type Props = {
+  specialistId: string;
+  specialistName: string;
+  city?: string | null;
+  category?: string | null;
+};
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+    fbq?: (...args: any[]) => void;
+  }
+}
+
+export default function TrackSpecialistClick(props: Props) {
+  const onClick = useCallback(() => {
+    window.gtag?.("event", "view_specialist", {
+      specialist_id: props.specialistId,
+      specialist_name: props.specialistName,
+      city: props.city ?? undefined,
+      category: props.category ?? undefined,
+    });
+
+    window.fbq?.("trackCustom", "ViewSpecialist", {
+      specialist_id: props.specialistId,
+      specialist_name: props.specialistName,
+      city: props.city ?? undefined,
+      category: props.category ?? undefined,
+    });
+  }, [props]);
+
+  return <span onClick={onClick} style={{ display: "contents" }} />;
+}
