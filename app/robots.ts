63cacd2e0c05e5f32b0950453e0ce6c3b8a47ae0
danyantonov66 <danyantonov66@gmail.com>
2026-03-10 07:18:45 +0200

    Update robots with sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
       disallow: [
+      Sitemap: https://prozona.bg/sitemap.xml
         "/api/",
         "/admin/",
         "/_next/",
