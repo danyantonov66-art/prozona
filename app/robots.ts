c77966d5707fb0fc60d1d883d556c3b2f3b5fb88
danyantonov66 <danyantonov66@gmail.com>
2026-03-10 07:21:00 +0200

    Fix robots syntax

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
       disallow: [
-      Sitemap: https://prozona.bg/sitemap.xml
         "/api/",
         "/admin/",
         "/_next/",
