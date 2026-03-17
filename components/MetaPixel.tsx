5b73f80c5137887d2ee640348f03398fd22737e0
danyantonov66 <danyantonov66@gmail.com>
2026-03-17 18:21:27 +0200

    fix: direct env access for meta pixel

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -1,22 +1,6 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import Script from 'next/script'
-import { useEffect, Suspense } from 'react'
-
-function PixelEvents() {
-  const pathname = usePathname()
-  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID
-
-  useEffect(() => {
-    if (!pixelId || !window.fbq) return
-    
-    // Отчитаме PageView само при промяна на URL
-    window.fbq('track', 'PageView')
-  }, [pathname, pixelId])
-
-  return null
-}
 
 export default function MetaPixel() {
   const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID
@@ -25,33 +9,31 @@ export default function MetaPixel() {
 
   return (
     <>
-      <Script id="meta-pixel-base" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${pixelId}');
-          fbq('track', 'PageView');
-        `}
-      </Script>
-      
-      {/* Махаме useSearchParams, за да не чупим блога */}
-      <Suspense fallback={null}>
-        <PixelEvents />
-      </Suspense>
-
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
       <noscript>
         <img
           height="1"
           width="1"
           style={{ display: 'none' }}
-          alt=""
           src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          alt=""
         />
       </noscript>
     </>
