aa5a245827a21651e35a17631165cd753d30a002
danyantonov66 <danyantonov66@gmail.com>
2026-03-18 07:27:29 +0200

    fix auth + pixel

--- lib/auth.ts
+++ lib/auth.ts
@@ -49,33 +49,32 @@ export const authOptions: NextAuthOptions = {
     })
   ],
   callbacks: {
-  async jwt({ token, user }) {
-    // при login
-    if (user) {
-      token.id = user.id
-      token.role = user.role ?? "user"
-    }
-
-    // 🔥 винаги взимаме role от базата
-    if (token.id) {
-      const dbUser = await prisma.user.findUnique({
-        where: { id: token.id as string },
-        select: { role: true },
-      })
-
-      if (dbUser) {
-        token.role = dbUser.role
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.role = user.role ?? "user"
       }
-    }
 
-    return token
-  },
+      if (token.id) {
+        const dbUser = await prisma.user.findUnique({
+          where: { id: token.id as string },
+          select: { role: true },
+        })
 
-  async session({ session, token }) {
-    if (session.user) {
-      session.user.id = token.id as string
-      session.user.role = token.role as Role
-    }
-    return session
+        if (dbUser) {
+          token.role = dbUser.role
+        }
+      }
+
+      return token
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.role = token.role as Role
+      }
+      return session
+    },
   },
 }
