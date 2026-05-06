d57f9606ae880e658e694fb759a71d37988a561a
danyantonov66 <danyantonov66@gmail.com>
2026-05-06 14:30:21 +0300

    perf: add remotePatterns for Next.js Image

--- next.config.js
+++ next.config.js
@@ -13,6 +13,12 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200],
     minimumCacheTTL: 60 * 60 * 24 * 30,
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "lh4.googleusercontent.com" },
+      { protocol: "https", hostname: "utfs.io" },
+      { protocol: "https", hostname: "uploadthing.com" },
+    ],
   },
   compress: true,
   poweredByHeader: false,
