3f26e3d2ce0bb720a9b2f67ccd7426f5cc748cd9
danyantonov66 <danyantonov66@gmail.com>
2026-05-23 07:21:10 +0300

    fix: remove duplicate GA4 from locale layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import { Providers } from '../providers'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] })
 
@@ -14,22 +13,8 @@ export default async function LocaleLayout({
   const { locale } = await params
 
   return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-W3JYCB8NJS"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-W3JYCB8NJS');
-        `}
-      </Script>
-      <Providers>
-        {children}
-      </Providers>
-    </>
+    <Providers>
+      {children}
+    </Providers>
   )
 }
